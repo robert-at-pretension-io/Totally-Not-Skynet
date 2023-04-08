6d690206cbc4a93c13a86288c81a74a8b5a4cfd9
Robert Elliot Pahel-Short <rep@akc.org>
2023-04-08 10:11:00 -0400

    ok, websockets works now

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,14 +1,14 @@
+use futures_util::{SinkExt, StreamExt};
+use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::BufReader;
+use std::path::Path;
+use std::sync::Arc;
 use tokio::net::TcpListener;
 use tokio::sync::{mpsc, Mutex};
 use tokio::time::{self, Duration};
 use tokio_tungstenite::tungstenite::Message;
-use futures_util::{SinkExt, StreamExt};
-use std::sync::Arc;
-use std::fs::File;
-use std::io::BufReader;
-use std::path::Path;
 use walkdir::WalkDir;
-use serde::{Serialize, Deserialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Action {
@@ -17,13 +17,24 @@ struct Action {
     system: String,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+struct Process {
+    name: String,
+    trigger: String,
+    triggers_next_process: String,
+    waits_for_branch_completion: String,
+    steps: Vec<String>,
+    description: String,
+    creates_process_branch: String,
+    branch_step: String,
+}
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 struct Identity {
-    name: String
+    name: String,
 }
 
-impl Identity{
+impl Identity {
     fn new(name: String) -> Identity {
         Identity { name }
     }
@@ -32,7 +43,10 @@ impl Identity{
     }
 }
 
-async fn start_websocket_server(rx: Arc<Mutex<mpsc::Receiver<(Identity, Message)>>>, client_tx: mpsc::Sender<(Identity, String)>) {
+async fn start_websocket_server(
+    rx: Arc<Mutex<mpsc::Receiver<(Identity, Message)>>>,
+    client_tx: mpsc::Sender<(Identity, String)>,
+) {
     let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
 
     while let Ok((stream, addr)) = listener.accept().await {
@@ -40,8 +54,9 @@ async fn start_websocket_server(rx: Arc<Mutex<mpsc::Receiver<(Identity, Message)
         let client_tx = client_tx.clone();
         // Spawn a new task for each incoming connection
         tokio::spawn(async move {
-
-            let mut this_client = Identity { name: "".to_string() };
+            let mut this_client = Identity {
+                name: "test".to_string(),
+            };
             let this_client_clone = this_client.clone();
 
             let ws_stream = match tokio_tungstenite::accept_async(stream).await {
@@ -55,11 +70,14 @@ async fn start_websocket_server(rx: Arc<Mutex<mpsc::Receiver<(Identity, Message)
 
             let (mut outgoing, mut incoming) = ws_stream.split();
 
+            let cloned_client = this_client.clone();
+
             // Send/receive actions, processes, and messages by reading from the channel
             tokio::spawn(async move {
+                
                 while let Some(outgoing_msg) = rx.lock().await.recv().await {
                     // check if the client has the same identity as the message
-                    if outgoing_msg.0.name == this_client.name {
+                    if outgoing_msg.0.name == cloned_client.name {
                         outgoing.send(outgoing_msg.1.clone()).await.unwrap();
                     }
                 }
@@ -69,14 +87,21 @@ async fn start_websocket_server(rx: Arc<Mutex<mpsc::Receiver<(Identity, Message)
             while let Some(msg) = incoming.next().await {
                 match msg {
                     Ok(msg) => {
-                        println!("Received a message from {}: {}", addr, msg.to_text().unwrap());
+                        println!(
+                            "Received a message from {}: {}",
+                            addr,
+                            msg.to_text().unwrap()
+                        );
 
                         // if this_client is not set, then set it
                         if this_client_clone.check_equal_to_string("".to_string()) {
                             this_client.name = msg.to_text().unwrap().to_string();
                         }
 
-                        client_tx.send((this_client.clone(), msg.to_string())).await.unwrap();
+                        client_tx
+                            .send((this_client.clone(), msg.to_string()))
+                            .await
+                            .unwrap();
                     }
                     Err(e) => {
                         println!("Error processing message from {}: {:?}", addr, e);
@@ -87,30 +112,44 @@ async fn start_websocket_server(rx: Arc<Mutex<mpsc::Receiver<(Identity, Message)
         });
     }
 }
-use mongodb::{Client, options::ClientOptions};
-async fn start_mongo_client() {
-    Client::with_uri_str("mongodb://localhost:27017/").await.unwrap();
+use mongodb::{options::ClientOptions, Client};
+async fn return_db() -> mongodb::Database {
+    let client = Client::with_uri_str("mongodb://localhost:27017/")
+        .await
+        .unwrap();
+
+    client.database("skynet")
 }
 
+async fn start_message_sending_loop(
+    tx: mpsc::Sender<(Identity, Message)>,
+    mut client_rx: mpsc::Receiver<(Identity, String)>,
+    my_actions: Vec<Action>,
+) {
+    //read messages from the client
+    while let Some(msg) = client_rx.recv().await {
+        println!("Received a message from the client: {}", msg.1);
 
-async fn start_message_sending_loop(tx: mpsc::Sender<(Identity, Message)>, client_rx : mpsc::Receiver<(Identity, String)>, my_actions: Vec<Action>) {
-
-        //read messages from the client
-        while let Some(msg) = client_rx.recv().await {
-            println!("Received a message from the client: {}", msg.1);
-
-            
-                for action in &my_actions {
-                    tx.send((Identity::new("test".to_string()),Message::Text(serde_json::to_string(&action).unwrap()))).await.unwrap();
-                }
+        for action in &my_actions {
+            tx.send((
+                Identity::new("test".to_string()),
+                Message::Text(serde_json::to_string(&action).unwrap()),
+            ))
+            .await
+            .unwrap();
         }
-        
-        loop {
-            tx.send((Identity::new("test".to_string()), Message::Text("Hello World".to_string()))).await.unwrap();
-            time::sleep(Duration::from_secs(10)).await;
-        }
+    }
+
+    loop {
+        tx.send((
+            Identity::new("test".to_string()),
+            Message::Text("Hello World".to_string()),
+        ))
+        .await
+        .unwrap();
+        time::sleep(Duration::from_secs(10)).await;
+    }
 }
-
 
 fn read_json_file<P: AsRef<Path>>(path: P) -> Result<Action, Box<dyn std::error::Error>> {
     let file = File::open(path)?;
@@ -125,7 +164,9 @@ fn import_actions(directory: &str) -> Result<Vec<Action>, Box<dyn std::error::Er
     for entry in WalkDir::new(directory)
         .into_iter()
         .filter_map(Result::ok)
-        .filter(|e| e.file_type().is_file() && e.path().extension().map_or(false, |ext| ext == "json"))
+        .filter(|e| {
+            e.file_type().is_file() && e.path().extension().map_or(false, |ext| ext == "json")
+        })
     {
         let json_data = read_json_file(entry.path())?;
         results.push(json_data);
@@ -134,25 +175,27 @@ fn import_actions(directory: &str) -> Result<Vec<Action>, Box<dyn std::error::Er
     Ok(results)
 }
 
-
 #[tokio::main]
 async fn main() {
+    // get the database
+    let db = return_db().await;
+    let action_collection = db.collection::<Action>("actions");
+    let process_collection = db.collection::<Process>("processes");
 
+    let mut actions_cursor = action_collection.find(None, None).await.unwrap();
 
-    // In the future, these will be loaded from mongodb
-    let mut my_actions = vec![];
+    let mut processes_cursor = process_collection.find(None, None).await.unwrap();
 
-    match import_actions("./src/actions")
-        {
-            Ok(actions) => {my_actions = actions;
-                println!("actions loaded");
-},
-            Err(_) => todo!(),
-        }
+    let mut actions = Vec::new();
+    let mut processes = Vec::new();
 
-    let session = start_mongo_client().await;
+    while let Some(action) = actions_cursor.next().await {
+        actions.push(action.unwrap());
+    }
 
-    
+    while let Some(process) = processes_cursor.next().await {
+        processes.push(process.unwrap());
+    }
 
     let (tx, rx) = mpsc::channel(100);
     let rx = Arc::new(Mutex::new(rx));
@@ -166,7 +209,7 @@ async fn main() {
 
     // Spawn the message sender task
     let sender_task = tokio::spawn(async move {
-        start_message_sending_loop(tx, client_rx, my_actions).await;
+        start_message_sending_loop(tx, client_rx, actions).await;
     });
 
     // Wait for both tasks to complete
