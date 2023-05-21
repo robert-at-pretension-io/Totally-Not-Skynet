66a0ccf0777aa93395c7b6b95544947105f230c0
Robert Elliot Pahel-Short <rep@akc.org>
2023-05-21 12:13:52 -0400

    burp

--- server_reset.py
+++ server_reset.py
@@ -3,28 +3,34 @@ import socket
 import subprocess
 from signal import SIGTERM
 
+
 def kill_process(name):
     print(f"Attempting to kill all {name} processes...")
     subprocess.run(['pkill', '-f', name])
     print(f"All {name} processes killed.")
+
 
 def git_pull(path):
     print(f"Starting git pull in {path}...")
     subprocess.run(['git', '-C', path, 'pull'])
     print(f"Git pull in {path} completed.")
 
+
 def run_npm(path):
     print(f"Starting npm run dev in {path}...")
     subprocess.Popen(['npm', 'run', 'dev'], cwd=path)
     print(f"Npm run dev in {path} started.")
+
 
 def run_cargo(path):
     print(f"Starting cargo run in {path}...")
     subprocess.Popen(['cargo', 'run'], cwd=path)
     print(f"Cargo run in {path} started.")
 
+
 def listen_on_port(port, message):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.bind(('0.0.0.0', port))
         s.listen()
         print(f"Listening on port {port}...")
@@ -38,6 +44,7 @@ def listen_on_port(port, message):
                 elif data.decode("utf-8") == message:
                     return True
     return False
+
 
 while True:
     if listen_on_port(420, "reset"):
