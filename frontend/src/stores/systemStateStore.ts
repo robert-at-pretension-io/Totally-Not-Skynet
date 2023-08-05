2a2c27c43c9f594a5160c3b943ae0bbda0bb2884
elliot <robert@pretension.io>
2023-08-05 13:43:46 -0400

    trying to use the url instead of the ip address

--- frontend/src/stores/systemStateStore.ts
+++ frontend/src/stores/systemStateStore.ts
@@ -6,7 +6,7 @@ const system_state: SystemState = {
   websocketReady: false,
   selectedNode: null,
   graphState: newGraphState(),
-  websocket: new WebSocket("ws://134.122.112.104:8080"),
+  websocket: new WebSocket("ws://liminalnook.com:8080"),
   executionContext: NewExecutionContext(),
   nodes: [],
 };
