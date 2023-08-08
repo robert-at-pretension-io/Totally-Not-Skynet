4ec57d62eaa1e2f1fdb83bd79c00a5abea7e6e8c
elliot <robert@pretension.io>
2023-08-08 17:45:51 -0400

    Crying is a very human thing...

--- backend/src/check_if_docker_installed.rs
+++ backend/src/check_if_docker_installed.rs
@@ -1,25 +1,35 @@
 use std::process::Command;
 use std::fs;
+use std::io;
+use anyhow::Result;
+use thiserror::Error;
 
-pub fn docker_check() {
+#[derive(Error, Debug)]
+pub enum DockerCheckError {
+    #[error("Not running Ubuntu, can't install Docker automatically.")]
+    NotUbuntu,
+    #[error("Command execution failed: {0}")] CommandFailed(String),
+}
+
+pub fn docker_check() -> Result<()> {
     // Check if Docker is installed
-    let output = Command::new("docker").arg("--version").output();
+    let output = Command::new("docker").arg("--version").output()?;
 
-    if output.is_ok() {
-        println!("Docker is installed: {:?}", output.unwrap());
+    if output.status.success() {
+        println!("Docker is installed: {:?}", output);
+        return Ok(());
+    } else if is_ubuntu() {
+        println!("Docker is not installed. Also, you're running ubuntu! Installing...");
+        install_docker()?;
+        return Ok(());
     } else {
-        if is_ubuntu() {
-            println!("Docker is not installed. Also, you're running ubuntu! Installing...");
-            install_docker()
-        } else {
-            panic!("Not running ubuntu, can't install docker automatically.")
-        }
+        return Err(DockerCheckError::NotUbuntu.into());
     }
 }
 
-fn install_docker() {
+fn install_docker() -> Result<()> {
     // Step 1: Update existing list of packages
-    run_command("sudo", &["apt-get", "update"]);
+    run_command("sudo", &["apt-get", "update"])?;
 
     // Step 2: Install prerequisites
     run_command(
@@ -33,64 +43,55 @@ fn install_docker() {
             "curl",
             "software-properties-common",
         ]
-    );
+    )?;
 
     // Step 3: Add Docker's official GPG key
-    let cmd_str = "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -";
-    run_shell_command(cmd_str);
+    run_shell_command(
+        "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -"
+    )?;
 
-    let output = Command::new("lsb_release")
-        .arg("-cs")
-        .output()
-        .expect("Failed to execute lsb_release");
+    let output = Command::new("lsb_release").arg("-cs").output()?;
     let codename = String::from_utf8_lossy(&output.stdout).trim().to_string();
 
     // Step 4: Set up the Docker stable repository
     let repo_url =
         format!("deb [arch=amd64] https://download.docker.com/linux/ubuntu {} stable", codename);
-    run_command("sudo", &["add-apt-repository", &repo_url]);
+    run_command("sudo", &["add-apt-repository", &repo_url])?;
 
     // Step 5: Update the apt package index (again)
-    run_command("sudo", &["apt-get", "update"]);
+    run_command("sudo", &["apt-get", "update"])?;
 
     // Step 6: Install Docker
-    run_command("sudo", &["apt-get", "install", "-y", "docker-ce"]);
+    run_command("sudo", &["apt-get", "install", "-y", "docker-ce"])?;
 
     println!("Docker installed successfully");
+    Ok(())
 }
 
-fn run_shell_command(command_str: &str) {
-    let status = Command::new("sh")
-        .arg("-c")
-        .arg(command_str)
-        .status()
-        .expect(&format!("Failed to run shell command: {}", command_str));
+fn run_shell_command(command_str: &str) -> Result<()> {
+    let status = Command::new("sh").arg("-c").arg(command_str).status()?;
 
-    if !status.success() {
-        panic!("Shell command failed: {}", command_str);
+    if status.success() {
+        Ok(())
+    } else {
+        Err(DockerCheckError::CommandFailed(command_str.to_string()).into())
     }
 }
 
-fn run_command(command: &str, args: &[&str]) {
-    let status = Command::new(command)
-        .args(args)
-        .status()
-        .expect(&format!("Failed to run command: {}", command));
+fn run_command(command: &str, args: &[&str]) -> Result<()> {
+    let status = Command::new(command).args(args).status()?;
 
-    if !status.success() {
-        panic!("Command failed: {}", command);
+    if status.success() {
+        Ok(())
+    } else {
+        Err(DockerCheckError::CommandFailed(command.to_string()).into())
     }
 }
 
 fn is_ubuntu() -> bool {
     // Read the contents of the /etc/os-release file
-    let contents = match fs::read_to_string("/etc/os-release") {
-        Ok(contents) => contents,
-        Err(_) => {
-            return false;
-        }
-    };
-
-    // Check if the contents contain the "Ubuntu" identifier
-    contents.contains("NAME=\"Ubuntu\"")
+    match fs::read_to_string("/etc/os-release") {
+        Ok(contents) => contents.contains("NAME=\"Ubuntu\""),
+        Err(_) => false,
+    }
 }
