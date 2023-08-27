1e66059629fb5db16f8804d5b6b5639bf9d63638
Robert Pahel-Short <robert@pretension.io>
2023-08-27 12:20:44 -0400

    Add quality of life console logging

--- backend/src/env_vars_checker.rs
+++ backend/src/env_vars_checker.rs
@@ -1,3 +1,6 @@
+extern crate colored;
+
+use colored::*;
 use std::env;
 use std::fs::File;
 use std::io::{self, BufRead, Error, ErrorKind};
@@ -11,11 +14,14 @@ pub fn check_env_vars(file_location: &str) -> io::Result<()> {
     for line in reader.lines() {
         let var_name = line?;
         match env::var(&var_name) {
-            Ok(_) => println!("{} is set.", var_name),
+            Ok(_) => println!("{} : {} is set.", "✅ Success!".green(), var_name),
             Err(env::VarError::NotPresent) => {
-                println!("{} is NOT set.", var_name);
-                return Err(Error::new(ErrorKind::Other, format!("{} is not set.", var_name)));
-            },
+                println!("{} : {} is NOT set.", "❌ Failure!".red(), var_name);
+                return Err(Error::new(
+                    ErrorKind::Other,
+                    format!("{} is not set.", var_name),
+                ));
+            }
             Err(e) => return Err(Error::new(ErrorKind::Other, e)),
         }
     }
