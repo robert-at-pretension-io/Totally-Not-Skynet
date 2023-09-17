a6b60dd401261fc2456c047882abb517757c06b8
elliot <robert@pretension.io>
2023-09-17 11:29:10 -0400

    change build.rs

--- backend/build.rs
+++ backend/build.rs
@@ -1,9 +1,11 @@
+extern crate prost_build;
+
 fn main() {
     println!("Change in build !!!!!!!!");
 
     prost_build
         ::compile_protos(&["../common/protobuf/system_types.proto"], &["../common/protobuf"])
-        .unwrap();
+        .expect("Failed to compile protos");
 
     println!("Should compile types to: {:?}", std::env::var("OUT_DIR"));
 }
